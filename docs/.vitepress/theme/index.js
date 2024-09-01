d2cf81e71a223092047116de2e3984bffbf4e063
Wes Copeland <wlcopeland1@gmail.com>
2024-09-01 10:33:54 -0400

    feat: add git-driven automated changelogs to every page

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,3 +1,12 @@
+import DefaultTheme from "vitepress/theme";
+import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
+import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
+
 import "./styles.css";
 
-export { default } from "vitepress/theme";
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.use(NolebaseGitChangelogPlugin);
+  },
+};
