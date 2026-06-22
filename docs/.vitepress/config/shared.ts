c57989469b4eb705fffd8553c0afdca0238a9506
Wes Copeland <wlcopeland1@gmail.com>
2026-06-22 12:15:42 -0400

    chore: remove stale translations

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -1,8 +1,5 @@
 import { type HeadConfig, defineConfig } from "vitepress";
 
-import { search as esSearch } from "./es";
-import { search as ptSearch } from "./pt";
-
 export const shared = defineConfig({
   // Metadata
   title: "RetroAchievements",
@@ -34,7 +31,6 @@ export const shared = defineConfig({
         appId: "ZZXWI82V1J",
         apiKey: "2b510f2ef4d79811fe1f6dd437c595e1",
         indexName: "retroachievements",
-        locales: { ...esSearch, ...ptSearch },
       },
     },
 
